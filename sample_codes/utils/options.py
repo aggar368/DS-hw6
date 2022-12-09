afb450b8c7bd0f6bf8ee66a7e8dc1b8a8571f5c0
aggar368 <57944323+aggar368@users.noreply.github.com>
2022-12-09 15:00:49 +0800

    Add files via upload

--- sample_codes/utils/options.py
+++ sample_codes/utils/options.py
@@ -38,7 +38,7 @@ parser.add_argument('--train_batch_size', type = int, default = 32, help = 'Batc
 parser.add_argument('--eval_batch_size', type = int, default = 32, help = 'Batch size for validation.')
 
 parser.add_argument('--momentum', type = float, default = 0.9, help = 'Momentum for MomentumOptimizer.')
-parser.add_argument('--lr', type = float, default = 0.003) #
+parser.add_argument('--lr', type = float, default = 0.007) #
 parser.add_argument('--lr_gamma', type = float, default = 0.1)
 parser.add_argument('--lr_decay_step', type = int, default = 35)
 
