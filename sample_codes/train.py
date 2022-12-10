9cb5eb821ae17acdeff8d2f021fdaf8021a4adc9
aggar368 <57944323+aggar368@users.noreply.github.com>
2022-12-11 03:46:31 +0800

    Add files via upload

--- sample_codes/train.py
+++ sample_codes/train.py
@@ -65,13 +65,13 @@ def main():
 
     param = [param for name, param in model.named_parameters()]
     
-    optimizer = optim.Adam(param, lr = args.lr, betas=(0.9, 0.999), eps=1e-08)
-    #optimizer = optim.SGD(param, lr = args.lr, momentum = args.momentum, weight_decay = args.weight_decay)
-    #scheduler = StepLR(optimizer, args.lr_decay_step, gamma = args.lr_gamma)
+    #optimizer = optim.Adam(param, lr = args.lr, betas=(0.9, 0.999), eps=1e-08)
+    optimizer = optim.SGD(param, lr = args.lr, momentum = args.momentum, weight_decay = args.weight_decay)
+    scheduler = StepLR(optimizer, args.lr_decay_step, gamma = args.lr_gamma)
 
 
     for epoch in range(start_epoch, args.num_epochs):
-        #scheduler.step(epoch)
+        scheduler.step(epoch)
         
         train(args, data_loader, model, optimizer, epoch)
         
