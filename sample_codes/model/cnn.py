818916968f258b20b748392e57b91a6c5ec1d2b1
aggar368 <57944323+aggar368@users.noreply.github.com>
2022-12-13 01:09:34 +0800

    Add files via upload

--- sample_codes/model/cnn.py
+++ sample_codes/model/cnn.py
@@ -2,6 +2,7 @@ import torch
 import torch.nn as nn
 from torch.autograd import Function
 import torch.nn.functional as F
+import math
 
 from utils.options import args
 
@@ -17,39 +18,56 @@ class CNN(nn.Module):
         super(CNN, self).__init__()
         
      
-        self.conv1 = nn.Conv2d(3, 32, kernel_size=3, stride=1, padding=1, bias=False)
-        self.relu = nn.ReLU(inplace=True)
-        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1, bias=False)
-        self.dropout1 = nn.Dropout(0.25)
-        self.dropout2 = nn.Dropout(0.2)
-        self.fc1 = nn.Linear(12544, 128)
-        self.fc2 = nn.Linear(128, num_classes)
+        self.features = nn.Sequential(
+            nn.Conv2d(3, 32, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(32),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(32, 32, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(32),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Conv2d(32, 64, kernel_size=3, padding=1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(64, 64, kernel_size=3, padding=1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2)
+        )
+          
+        self.classifier = nn.Sequential(
+            nn.Dropout(p = 0.5),
+            nn.Linear(64 * 7 * 7, 512),
+            nn.BatchNorm1d(512),
+            nn.ReLU(inplace=True),
+            nn.Dropout(p = 0.5),
+            nn.Linear(512, 512),
+            nn.BatchNorm1d(512),
+            nn.ReLU(inplace=True),
+            nn.Dropout(p = 0.5),
+            nn.Linear(512, num_classes),
+        )
 
-        for m in self.modules():
+        for m in self.features.children():
             if isinstance(m, nn.Conv2d):
-                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
-            elif isinstance(m, (nn.BatchNorm2d, nn.GroupNorm)):
-                nn.init.constant_(m.weight, 1)
-                nn.init.constant_(m.bias, 0)
+                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+                m.weight.data.normal_(0, math.sqrt(2. / n))
+            elif isinstance(m, nn.BatchNorm2d):
+                m.weight.data.fill_(1)
+                m.bias.data.zero_()
+        
+        for m in self.classifier.children():
+            if isinstance(m, nn.Linear):
+                nn.init.xavier_uniform(m.weight)
+            elif isinstance(m, nn.BatchNorm1d):
+                m.weight.data.fill_(1)
+                m.bias.data.zero_()
 
 
     def forward(self, x):
-        x = F.relu(self.conv1(x))
-
-        x = F.relu(self.conv2(x))
+        x = self.features(x)
+        x = x.view(x.size(0), -1)
+        x = self.classifier(x)
         
-        x = F.max_pool2d(x, 2)
-
-        x = self.dropout1(x)
-
-        feature = torch.flatten(x, 1)
-        #print(feature.shape)
-        x = F.relu(self.fc1(feature))
-
-        x = self.dropout2(x)
-
-        x = self.fc2(x)
-        
-
-        return x
+        return x 
 
