8ae27c046997f6063a66491c3ebf7ab482a9d9e8
aggar368 <57944323+aggar368@users.noreply.github.com>
2022-12-14 14:24:45 +0800

    Add files via upload

--- sample_codes/data/dataPreparer.py
+++ sample_codes/data/dataPreparer.py
@@ -15,17 +15,28 @@ import random
 
 class DataPreparation(Dataset):
     def __init__(self, root=args, data_path=None, label_path=None,
-                 transform=None, target_transform=None):
+                 transform=None, target_transform=None, extra_path=None, 
+                 extra_label_path=None, extra_num=None, reduced_num=None):
         
         self.root = root
         self.data_path = data_path 
         self.label_path = label_path 
         
+        # Extra data from valid set for train set
+        # TODO
+        self.extra_path = extra_path
+        self.extra_label_path = extra_label_path
+        self.extra_num = extra_num
+        self.reduced_num = reduced_num
+
+        
         self.transform = transform
         self.target_transform = target_transform
         
         ## preprocess files
-        self.preprocess(self.data_path, self.label_path)
+        self.preprocess(self.data_path, self.label_path, self.extra_path, 
+                        self.extra_label_path, extra_num=self.extra_num, 
+                        reduced_num=self.reduced_num)
         
 
     def __len__(self):
@@ -49,12 +60,27 @@ class DataPreparation(Dataset):
 
         return image, label, data_file
     
-    def preprocess(self, data_path, label_path):
+    def preprocess(self, data_path, label_path, extra_path, extra_label_path, 
+                    extra_num=None, reduced_num=None):
         self.data_files = os.listdir(data_path)
+        if reduced_num is not None:   # for valid set
+            self.data_files = self.data_files[reduced_num:]
+
+        if extra_path is not None:   # for train set
+            extra = os.listdir(extra_path)
+            self.data_files = self.data_files + extra[:extra_num]
+
         self.data_files.sort()
   
-        if label_path is not None:
+        # label-setting part
+        if label_path is not None:   # for train&valid set
             self.file_labels = pd.read_csv(label_path)
+            if extra_label_path is not None:   # for train set
+                extra_labels = pd.read_csv(extra_label_path)
+                self.file_labels = pd.concat([self.file_labels, extra_labels[:extra_num]], ignore_index=True)
+            else:                              # for valid set
+                self.file_labels = self.file_labels[reduced_num:]
+                self.file_labels.reset_index(drop=True, inplace=True)
         
 
 class RandomShift(object):
@@ -78,7 +104,7 @@ class RandomShift(object):
 
 
 class Data:
-    def __init__(self, args, data_path, label_path):
+    def __init__(self, args, data_path, label_path, move):
         
 
         transform = transforms.Compose([
@@ -95,10 +121,17 @@ class Data:
             transforms.Normalize(mean=(0.5), std=(0.5))
         ])
        
+        # Validation Data Position
+        valid_data_path = data_path.replace('train', 'valid')
+        valid_label_path = label_path.replace('train', 'valid')
+        
         # Train Data
         train_dataset = DataPreparation(root=args,  
                                         data_path=data_path,
                                         label_path=label_path,
+                                        extra_path=valid_data_path,
+                                        extra_label_path=valid_label_path,
+                                        extra_num=move,
                                         transform=transform)
         
         self.loader_train = DataLoader(
@@ -106,13 +139,11 @@ class Data:
             num_workers=2
             )
         
-        # Validation Data
-        valid_data_path = data_path.replace('train', 'valid')
-        valid_label_path = label_path.replace('train', 'valid')
         
         valid_dataset = DataPreparation(root=args,  
                                        data_path=valid_data_path,
                                        label_path=valid_label_path,
+                                       reduced_num=move,
                                        transform=transform)
         
         self.loader_valid = DataLoader(
